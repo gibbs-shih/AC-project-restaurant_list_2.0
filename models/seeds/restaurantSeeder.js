d0e7c5ed7be1671d5eeb2539e6379b1d41c3b4ca
gibbs-shih <shih.gibbs@gmail.com>
2023-05-11 17:31:22 +0800

    refactor: modify mongoose config

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,13 @@
-const mongoose = require('mongoose')
+// 載入mongoose設定
+const db = require('../../config/mongoose')
+
 // 載入 Restaurant model
 const Restaurant = require('../restaurant.js')
+
 const seedInfo = require('/Users/gibbsshih/project_restaurant_list/restaurant.json')
 const seedInfoList = seedInfo.results
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
 
+db.once('open', () => {
    // 建立種子資料
    Restaurant.create(seedInfoList)
   
