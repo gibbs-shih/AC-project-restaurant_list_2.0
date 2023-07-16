7a30246a44d3c580eb648f8bdb04cee617b25952
gibbs-shih <shih.gibbs@gmail.com>
2023-07-16 09:14:11 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // 準備引入路由模組
 const home = require('./modules/home') // 引入 home 模組程式碼
-router.use('/', home) // 將網址結構符合 / 字串的 request 導向 home 模組
-
 const restaurants = require('../routes/modules/restaurants') // 引入 restaurants 模組程式碼
+const users = require('./modules/users') // 引入 users 模組程式碼
+
+router.use('/users', users)  // / 將網址結構符合 /users 字串開頭的 request 導向 users 模組 
 router.use('/restaurants', restaurants) // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
+router.use('/', home) // 將網址結構符合 / 字串的 request 導向 home 模組
 
 // 匯出路由器
 module.exports = router
